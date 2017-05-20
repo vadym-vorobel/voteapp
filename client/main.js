c714dd4dfa8f35ddbe39472f11a345a8a5a0465c
Vadym Vorobel <vadym.vorobel@gmail.com>
2017-05-20 23:00:20 +0300

    Change theme

--- client/main.js
+++ client/main.js
@@ -3,4 +3,4 @@ import '../imports/startup/client';
 import './main.html';
 
 import './main.css';
-import 'react-md/dist/react-md.indigo-pink.min.css';
+import 'react-md/dist/react-md.deep_orange-deep_purple.min.css';
