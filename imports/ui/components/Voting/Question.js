b46435d03e02d65aa98258f97d4db15c14d372a0
Vadym Vorobel <vadym.vorobel@gmail.com>
2017-09-17 11:16:45 +0300

    Add count of voters

--- imports/ui/components/Voting/Question.js
+++ imports/ui/components/Voting/Question.js
@@ -46,7 +46,7 @@ const getChartConfig = (answers) => ({
       cursor: 'pointer',
       dataLabels: {
         enabled: true,
-        format: '<b>{point.name}</b>: {point.percentage:.1f} %',
+        format: '<b>{point.name}</b>: {point.percentage:.1f} % ({y})',
       },
     },
   },
